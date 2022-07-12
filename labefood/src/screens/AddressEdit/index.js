0edc613c53a375ecb78eeb1c278f61704873ae87
gabriel antunes <gabriel.antunes@acad.pucrs.br>
2022-07-11 21:39:47 -0300

    adicionado screens, globalStyle e custom hook

---
+++ labefood/src/screens/AddressEdit/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Container} from './styles'
+
+const AddressEdit=()=>{
+  return (
+   <Container>
+
+   </Container>
+  );
+}
+
+export default AddressEdit;
---
+++ labefood/src/screens/RegisterAddress/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Container} from './styles'
+
+const RegisterAddress=()=>{
+  return (
+   <Container>
+
+   </Container>
+  );
+}
+
+export default RegisterAddress;
