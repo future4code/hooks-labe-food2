e586300d39d8eb08dbcb861ec87c2f771da0802b
Max-Ranholi <ranholimax@gmail.com>
2022-07-18 19:54:38 -0300

    cadastro

--- labefood/src/screens/SignUp/styles.js
+++ labefood/src/screens/SignUp/styles.js
@@ -1,5 +1,20 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+box-sizing: border-box;
+height: 740px;
+width: 360px;
+display: flex;
+align-items: center;
+justify-content: center;
+`
+export const FormSignUp = styled.form`
+display: flex;
+align-items: center;
+justify-content: center;
+flex-direction: column;
 
-`;
+input {
+    font-size: 20px;
+}
+`
