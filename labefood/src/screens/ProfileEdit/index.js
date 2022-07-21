a2c773eb113921fe48ddf9fa307261f92ba6e411
Max-Ranholi <ranholimax@gmail.com>
2022-07-21 20:21:02 -0300

    nenhuma alteração

--- labefood/src/screens/ProfileEdit/index.js
+++ labefood/src/screens/ProfileEdit/index.js
@@ -41,6 +41,7 @@ const ProfileEdit = () => {
         size="large"
         variant="contained"
         color="success"
+        pattern="([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})"
       >Salvar
       </Button>
     </Container>
