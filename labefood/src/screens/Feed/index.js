6ee324a15b19afa9464388edadecdcbc6c225a02
gabriel antunes <gabriel.antunes@acad.pucrs.br>
2022-07-23 16:26:52 -0300

    adicionado card de restaurante e input para pesquisa

--- labefood/src/screens/Feed/index.js
+++ labefood/src/screens/Feed/index.js
@@ -1,10 +1,24 @@
 import React from "react";
-import {Container} from './styles'
+import {Container} from './styles';
+import { TextField, InputAdornment } from "@mui/material";
+import SearchIcon from '../../assets/search.svg'
+
 
 const Feed=()=>{
   return (
    <Container>
-
+       <TextField
+        placeholder="Restaurante"
+        color="common"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <img src={SearchIcon} alt="search icon"/>
+            </InputAdornment>
+          ),
+        }}
+  
+      />
    </Container>
   );
 }
