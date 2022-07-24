b228c053b2c7a58b9f337ede94326c69aaf035a4
Walimr-Junior <juniorsouzafig@gmail.com>
2022-07-24 20:11:16 -0300

    pagina do carrinho inicio

--- labefood/src/screens/Cart/index.js
+++ labefood/src/screens/Cart/index.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import Footer from "../../components/Footer";
-import {Container} from './styles'
+import {URL_BASE, axiosConfig} from "../../constants/URL_BASE"
+import { ContainerFoter, Container, ContainerTitle, ContainerAdress, Title, DeliveryAddress, Address } from './styles'
 
-const Cart=()=>{
+const Cart = () => {
+  const [fullAddress, setFullAddress] = useState('')
+  const [hasRequest, setHasRequest] = useState(false)
+
+
+  const getFullAddress = () => {
+    axios
+    .get(`${URL_BASE}/profile/address`, axiosConfig)
+    .then(res => {
+      const address = `${res.data.address.street}, ${res.data.address.number}`
+      setFullAddress(address)
+    })
+    .catch(err => console.log(err))
+  }
+
+  getFullAddress()
   return (
-   <Container>
-     <Footer />
-   </Container>
+    
+      <Container>
+        <ContainerTitle>
+          <Title>Meu carrinho</Title>
+        </ContainerTitle>
+
+        <ContainerAdress>
+          <DeliveryAddress>Enderço de entrega</DeliveryAddress>
+          <Address>{fullAddress}</Address>
+        </ContainerAdress>
+
+        <ContainerFoter>
+          <Footer />
+        </ContainerFoter>
+      </Container>
   );
 }
 
--- labefood/src/screens/Cart/styles.js
+++ labefood/src/screens/Cart/styles.js
@@ -3,5 +3,70 @@ import styled from 'styled-components';
 export const Container = styled.div`
 height: 100vh;
 display: flex;
-align-items: flex-end;
+flex-direction: column;
+align-items: center;
 `;
+
+export const ContainerTitle = styled.div`
+    width: 100vw;
+    margin-top: 25px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+export const Title = styled.h3`
+  width: 91px;
+  height: 19px;
+  font-family: Roboto;
+  font-size: 16px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: -0.39px;
+  text-align: center;
+  color: black;
+`
+
+export const ContainerAdress = styled.div`
+    width: 360px;
+    height: 76px;
+    margin: 1px 0 8px;
+    padding: 16px;
+    background-color: #eee;
+    margin-top: 15px;
+`
+export const DeliveryAddress = styled.h3`
+    display: flex;
+    flex-direction: column;
+    width: 328px;
+    height: 18px;
+    margin: 0 0 8px;
+    font-family: Roboto;
+    font-size: 16px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: -0.39px;
+    color: #b8b8b8;
+`
+export const Address = styled.h3`
+    width: 328px;
+    height: 18px;
+    margin: 8px 0 0;
+    font-family: Roboto;
+    font-size: 16px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: -0.39px;
+    color: black;
+`
+
+export const ContainerFoter = styled.div`
+    width: 100vw;
+    bottom: 0;
+    position: fixed;
+`
