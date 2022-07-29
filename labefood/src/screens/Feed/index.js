98b725b886b640268d732dcbbcc24cbb52d0841c
gabriel antunes <gabriel.antunes@acad.pucrs.br>
2022-07-29 20:19:34 -0300

    adicionado filtro de categoria

--- labefood/src/screens/Feed/index.js
+++ labefood/src/screens/Feed/index.js
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState} from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import {URL_BASE} from '../../constants/URL_BASE'
-import {Container,RestaurantsList,Header} from './styles';
+import {Container,RestaurantsList,Header, ContainerCategories, CategoryItem} from './styles';
 import { TextField, InputAdornment } from "@mui/material";
 import SearchIcon from '../../assets/search.svg'
 import Footer from "../../components/Footer";
@@ -27,23 +27,43 @@ const [inputValue,setInputValue] = useState("")
 const [data,isLoading,error] = useRequestDataWithHeaders(url,headers)
 useEffect(()=>getCategoryList(),[data]) 
 const categories = []
+const [renderCategories, setRenderCategories] = useState([])
+const [categoryFilter, setCategoryFilter] = useState('')
+const [showCategoriesList, setShowCategoriesList] = useState(true)
 
 const chooseRestaurant = (id) =>{
   goToRestaurantDetails(navigate,id)
 }
 
 const handleInput = (e)=>{
+  setCategoryFilter('')
   setInputValue(e.target.value)
   getCategoryList()
+}
+
+const choiceCategory = (category) =>{
+  setCategoryFilter(category)
 }
 
 const getCategoryList = ()=>{data?.restaurants.map((restaurant)=>{
   const checkCategory = categories.find((produto)=> produto === restaurant.category)
   if(!checkCategory){
     categories.push(restaurant.category)}
-})}
+})
+  const categoriesList = data && categories && categories.map((item, index)=>{
+    return <CategoryItem 
+    key={index} 
+    onClick={()=>choiceCategory(item)}
+    visibility={showCategoriesList}
+    >{item}</CategoryItem>
+})
+  setRenderCategories(categoriesList)
+}
+
+
 
 const renderRestaurantsCards = data?.restaurants
+.filter((restaurant)=>{return restaurant.category.includes(categoryFilter)})
 .filter((restaurant)=>restaurant.name.toUpperCase().includes(inputValue.toUpperCase()))
 .map((restaurant)=>{
   return <CardRestaurant
@@ -55,6 +75,7 @@ const renderRestaurantsCards = data?.restaurants
   onClickCard={()=>chooseRestaurant(restaurant.id)}
   />
 })
+console.log(categories)
 
   return (
    <Container>
@@ -74,7 +95,9 @@ const renderRestaurantsCards = data?.restaurants
     
         />
       <RestaurantsList>
-        <ListCategory list={categories}/>
+        <ContainerCategories>
+          {renderCategories}
+        </ContainerCategories>
         {renderRestaurantsCards}
       </RestaurantsList>
       <Footer />
--- labefood/src/screens/Feed/styles.js
+++ labefood/src/screens/Feed/styles.js
@@ -26,3 +26,41 @@ export const Header = styled.div`
     width:100%;
     height:64px;
 `;
+
+export const ContainerCategories = styled.div`
+    width:360px;
+    height:42px;
+    display:flex;
+    margin-left:16px;
+    padding-right:16px;
+    margin-top:8px;
+    align-items:center;
+    overflow-x:scroll;
+     scrollbar-width: none;
+     -ms-overflow-style: none; 
+     &::-webkit-scrollbar{
+          width:0;
+          height:0;
+     }
+`;
+
+export const CategoryItem = styled.button`
+    all:unset;  
+    height: 18px;
+    margin: 0 8px;
+    font-family:inherit;
+    font-size:16px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: -0.39px;
+    text-align: center;
+    color:#000;
+    cursor:pointer;
+    display:${(props)=> props.visibility?`block`:`none` };
+
+    &:focus{
+        color:#5cb646;
+    }
+`;
